711ed322222402685c20fac40586df0fe3778117
Ivan Lartsov <lartsov@gmail.com>
2017-12-12 19:31:06 +0300

    packages fix

--- Futor/OptionsForm.Designer.cs
+++ Futor/OptionsForm.Designer.cs
@@ -34,9 +34,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(579, 356);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "OptionsForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Options";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OptionsForm_FormClosed);
             this.Load += new System.EventHandler(this.OptionsForm_Load);
             this.ResumeLayout(false);
